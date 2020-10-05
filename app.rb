be4948cc71943e98bd40b9d628ccb337da4d24f8
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-05 12:35:41 -0600

    Added notation to the lab

--- app.rb
+++ app.rb
@@ -3,10 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
+		#route to home
 		erb :index
+		#render home 
 	end
 
 	get "/info" do 
+		#route to info path
 		erb :info
+		#render the info.erb
 	end
 end
